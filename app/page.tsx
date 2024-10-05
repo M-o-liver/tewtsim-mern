8a83d4d147f206241012d16528e3608298484b75
M-o-liver <oliverbeancross@gmail.com>
2024-10-05 16:42:53 -0300

    Updated packaged. Changed title page for testing.

--- app/page.tsx
+++ app/page.tsx
@@ -9,12 +9,12 @@ export default function Home() {
           src="https://nextjs.org/icons/next.svg"
           alt="Next.js logo"
           width={180}
-          height={38}
+          height={38}w
           priority
         />
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
-            Get started by editing{" "}
+            Testing{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
               app/page.tsx
             </code>
