2a0d9aedc7f0eaeda09e3b272784a60c891db6a5
M-o-liver <oliverbeancross@gmail.com>
2024-10-06 16:45:01 -0300

    Work

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -3,6 +3,8 @@ import bcrypt from 'bcryptjs';
 import dbConnect from '@/lib/mongodb';
 import User from '@/models/User';
 
+export const dynamic = 'force-dynamic';
+
 export async function POST(req: Request) {
   await dbConnect();
 
