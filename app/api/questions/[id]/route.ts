ed1a2ef1f78828b1c857a55f792b55bba3c9be56
M-o-liver <oliverbeancross@gmail.com>
2024-10-06 11:34:01 -0300

    Fixed A/Q Route+Model

--- app/api/questions/[id]/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server'
-import dbConnect from '@/lib/mongodb'
-import Question from '@/models/Question'
-
-export async function GET(request: Request, { params }: { params: { id: string } }) {
-  await dbConnect()
-  const questions = await Question.find({ missionId: params.id })
-  return NextResponse.json(questions)
-}
