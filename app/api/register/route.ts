2fb65f374502868c5c06148394cbe6890720bd2e
M-o-liver <oliverbeancross@gmail.com>
2024-10-06 11:32:22 -0300

    Fix incl vercel DNS/Domain & MongoDB Networking

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -13,11 +13,14 @@ export async function POST(req: Request) {
   try {
     const user = await User.create({ username, password: hashedPassword });
     return NextResponse.json({ message: 'User created', userId: user._id }, { status: 201 });
-  } catch (error: any) {
-    if (error.code === 11000) {
-      return NextResponse.json({ error: 'User already exists' }, { status: 400 });
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      if ('code' in error && error.code === 11000) {
+        return NextResponse.json({ error: 'User already exists' }, { status: 400 });
+      }
+      console.error('Error creating user:', error);
+      return NextResponse.json({ error: 'An error occurred while creating the user' }, { status: 500 });
     }
-    console.error('Error creating user:', error);
-    return NextResponse.json({ error: 'An error occurred while creating the user' }, { status: 500 });
+    return NextResponse.json({ error: 'An unknown error occurred' }, { status: 500 });
   }
 }
